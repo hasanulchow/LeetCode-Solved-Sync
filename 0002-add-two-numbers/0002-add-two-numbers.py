a71811fef746d994286ad1c930382d08329fe1a9
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-11-08 21:40:19 -0500

    Time: 15 ms (5.29%), Space: 17 MB (19.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,24 +1,35 @@
+import itertools as it
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        dummy = ListNode()
-        res = dummy
-
-        total = carry = 0
-
-        while l1 or l2 or carry:
-            total = carry
-
-            if l1:
-                total += l1.val
-                l1 = l1.next
-            if l2:
-                total += l2.val
-                l2 = l2.next
-            
-            num = total % 10
-            carry = total // 10
-            dummy.next = ListNode(num)
-            dummy = dummy.next
-        
-        return res.next
+        def forward(curr = None):
+            if curr:
+                yield curr.val
+                for i in forward(curr.next):
+                    yield i
+        def add():
+            root = ListNode(-1,None)
+            ans = root
+            straightL1 = forward(l1)
+            straightL2 = forward(l2)
+            carry = 0
+            last = None
+            for num1, num2 in it.zip_longest(straightL1, straightL2, fillvalue=0):
+                val = carry+num1+num2
+                if val <= 9:
+                    carry = 0
+                    ans.val = val
+                    ans.next = ListNode(0, None)
+                    last = ans
+                    ans = ans.next
+                else:
+                    carry = val // 10
+                    val -= 10
+                    ans.val = val
+                    ans.next = ListNode(0, None)
+                    last = ans
+                    ans = ans.next
+            last.next = None
+            if carry:
+                last.next = ListNode(carry, None)
+            return root
+        return add()
