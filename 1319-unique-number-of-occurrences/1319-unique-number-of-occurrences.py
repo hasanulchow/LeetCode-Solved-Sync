bb479b2b511089f7efe400a0abd019b421bc2462
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-11-19 22:38:30 -0500

    Time: 28 ms (5.39%), Space: 17 MB (9.28%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,16 +1,5 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        dic = {}
-        for i in range(len(arr)):
-            if arr[i] in dic.keys():
-                dic[arr[i]]+=1
-            else:
-                dic[arr[i]]=1
 
-        lst=list(dic.values())
-        lst2=list(set(lst))
-        if len(lst)==len(lst2):
-            return True
-
-        return False
-        
+        l={arr.count(i):i for i in arr}
+        return len(l) == len(set(arr))
