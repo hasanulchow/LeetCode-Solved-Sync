0ce5ed98f65717242787d63871312d440d3f42fa
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-11-23 23:59:53 -0500

    Time: 0 ms (100%), Space: 16.7 MB (42.77%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -1,3 +1,29 @@
 class Solution:
-    def leafSimilar(self, r1: Optional[TreeNode], r2: Optional[TreeNode]) -> bool:
-        return (f:=lambda n:f(n.left)+f(n.right) or [n.val] if n else [])(r1)==f(r2)
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+
+        def dfs(root):                              #     Example: 
+                                                    #         root1:         root2:
+                                                    #            ______3__             __3__
+                                                    #           /         \           /     \
+                                                    #          5__         1         5       1__
+                                                    #         /   \       / \       / \     /   \
+                                                    #       *6*    2    *9* *8*   *6* *7* *4*    2
+                                                    #             / \                           / \
+                                                    #           *7* *4*                       *9* *8*
+                                                    #   root1:
+            if not root: return []                  #       node      dfs(root.left)+dfs(root.right)
+                                                    #       ––––      ––––––––––––––––––––––––––––––
+            if not root.left and not root.right:    #        2              [7] + [4]   = [7,4]
+                return [root.val]                   #        5              [6] + [7,4] = [6,7,4]
+                                                    #        1              [9] + [8]   = [9,8]
+            return dfs(root.left) + dfs(root.right) #        3          [6,7,4] + [9,8] = [6,7,4,9,8] <--
+                                                    #
+        return dfs(root1) == dfs(root2)             #   root2:
+                                                    #       node      dfs(root.left)+dfs(root.right)
+                                                    #       ––––      ––––––––––––––––––––––––––––––
+                                                    #        5            [6] + [7]     = [6,7]
+                                                    #        2            [9] + [8]     = [9,8]
+                                                    #        1            [4] + [9,8]   = [4,9,8]
+                                                    #        3          [6,7] + [4,9,8] = [6,7,4,9,8] <--
+
+                                                    #       Return True
