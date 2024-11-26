9c05469a98727c4a14eb4e4e6de4bc137aa00d6e
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-11-25 22:48:51 -0500

    Time: 0 ms (100%), Space: 17.1 MB (37.26%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,14 +1,23 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        if (not rooms): return False
+        # Edge case: if there are no rooms, return False
+        if not rooms: return False
+        
+        # Start with the keys in room 0 and mark room 0 as visited ('v')
         keys = rooms[0]
         rooms[0] = 'v'
-        while (keys):
-            key = keys.pop()
-            if rooms[key] != 'v':
-                keys += rooms[key]
-                rooms[key] = 'v'
+        
+        # Use a stack-like approach to explore rooms starting from the keys of room 0
+        while keys:
+            key = keys.pop()  # Take the last key from the stack
+            if rooms[key] != 'v':  # If the room hasn't been visited
+                keys += rooms[key]  # Add keys from the newly visited room to the stack
+                rooms[key] = 'v'  # Mark the room as visited
+        
+        # Check if all rooms have been visited. If any room is not visited, return False
         for room in rooms:
             if room != 'v': return False
-        return True
+        
+        return True  # If all rooms are visited, return True
+
         
