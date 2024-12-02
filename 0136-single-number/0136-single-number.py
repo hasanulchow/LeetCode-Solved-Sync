b5be8cc50874abe9e1187610dc5d24db5fec0ce8
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-01 23:23:36 -0500

    Time: 4 ms (49.21%), Space: 19.2 MB (17.69%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # Initialize a variable 'res' to 0, which will hold the result
         res = 0
-        for n in nums:
-            res = n^res
-        return res
         
+        # Iterate over each number in the input list 'nums'
+        for n in nums:
+            # Apply the XOR operation between 'n' and 'res'.
+            # XORing a number with itself results in 0 (n^n = 0)
+            # XORing a number with 0 results in the number itself (0^n = n)
+            # This way, pairs of identical numbers will cancel each other out, 
+            # and only the unique number will remain in 'res'.
+            res = n ^ res
+        
+        # Return the unique number that doesn't have a pair
+        return res
