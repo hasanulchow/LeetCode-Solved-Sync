f5d367e3ee276a6b2494cce9052fcde8b2c23eb4
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-07 23:42:42 -0500

    Time: 0 ms (100%), Space: 17.4 MB (10.8%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,8 +1,17 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # Step 1: Split the input string by spaces into a list of words
         arr = s.split()
+        
+        # Step 2: Initialize an empty list to store the reversed words
         res = []
-        for i in range(len(arr) -1, -1, -1):
+        
+        # Step 3: Loop through the list of words in reverse order
+        for i in range(len(arr) - 1, -1, -1):
+            # Append each word to the result list in reverse order
             res.append(arr[i])
+        
+        # Step 4: Join the words in the result list into a string with spaces between them
         return ' '.join(res)
+
         
