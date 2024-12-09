441407bce611e47684bdc870310fb855608853e8
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-08 23:44:43 -0500

    Time: 43 ms (10.22%), Space: 17.8 MB (6.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,13 +1,20 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        l=[[0 for i in range(len(t)+1) ] for j in range(len(s)+1)]
+        # Initialize a 2D list (matrix) to store the length of the longest common subsequence (LCS)
+        l = [[0 for i in range(len(t) + 1)] for j in range(len(s) + 1)]
+        
+        # Build the LCS matrix
         for i in range(len(s)):
             for j in range(len(t)):
-                if s[i]==t[j]:
-                    l[i+1][j+1]=l[i][j]+1
+                # If characters match, increment the subsequence length
+                if s[i] == t[j]:
+                    l[i + 1][j + 1] = l[i][j] + 1
+                # Otherwise, take the maximum subsequence length from previous computations
                 else:
-                    l[i+1][j+1]=max(l[i][j+1],l[i+1][j])
-        if len(s)==l[len(s)][len(t)]:
+                    l[i + 1][j + 1] = max(l[i][j + 1], l[i + 1][j])
+        
+        # If the length of the subsequence of 's' matches the LCS length at the bottom-right corner, return True
+        if len(s) == l[len(s)][len(t)]:
             return True
         else:
             return False
