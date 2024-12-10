92a239d0da89c1462839ee599e35a0bfd7addd5e
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-10 00:11:29 -0500

    Time: 277 ms (35.89%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
 
-SELECT * 
-FROM Cinema
+SELECT 
+    * 
+FROM 
+    Cinema
 WHERE
-MOD(id, 2) =1
-AND description != 'boring'
-ORDER BY rating DESC;
+    MOD(id, 2) = 1 -- Select rows where the `id` is odd
+    AND description != 'boring' -- Exclude rows where the `description` is 'boring'
+ORDER BY 
+    rating DESC; -- Sort the results by `rating` in descending order
