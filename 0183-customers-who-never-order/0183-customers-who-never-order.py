10f86484d51c311351ee6382810b5e6d204424c0
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-10 22:30:21 -0500

    Time: 272 ms (97.65%), Space: 67.1 MB (93.27%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function finds customers who have not placed any orders.
+    
+    Args:
+    customers (pd.DataFrame): A DataFrame containing customer details with at least columns 'id' and 'name'.
+    orders (pd.DataFrame): A DataFrame containing order details with at least a column 'customerId'.
+    
+    Returns:
+    pd.DataFrame: A DataFrame with a single column 'Customers' listing the names of customers who have not placed orders.
+    """
+    # Filter customers whose 'id' is not present in the 'customerId' column of the orders DataFrame
     df = customers[~customers['id'].isin(orders['customerId'])]
 
-    df = df[['name']].rename(columns={'name':'Customers'})
+    # Select only the 'name' column and rename it to 'Customers'
+    df = df[['name']].rename(columns={'name': 'Customers'})
 
+    # Return the resulting DataFrame
     return df
