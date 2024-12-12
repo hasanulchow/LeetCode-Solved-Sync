3cc0102cd6a92339ffbc5cbe841e0424c069f4d8
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-11 21:42:11 -0500

    Time: 0 ms (100%), Space: 17.2 MB (11.67%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,26 +1,37 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        # If the input list is empty, return it directly as there are no ranges to summarize.
         if not nums:
             return nums
+        
+        # Initialize the start and end of the current range.
         start = nums[0]
         end = nums[0]
-        res =[]
-        for i in range(1,len(nums),1):
-            print(i)
-            if nums[i] != nums[i-1] +1:
-                if start == end:
+        res = []  # List to store the summarized ranges.
+        
+        # Iterate through the list, starting from the second element.
+        for i in range(1, len(nums)):
+            print(i)  # Debugging line, can be removed after testing.
+            
+            # Check if the current number is not consecutive with the previous number.
+            if nums[i] != nums[i - 1] + 1:
+                # Add the current range to the result list.
+                if start == end:  # If the range consists of a single number.
                     res.append(f'{start}')
-                else:
+                else:  # If the range has multiple numbers.
                     res.append(f'{start}->{end}')
-                start=nums[i]
-                end=nums[i]
-            else:
-                end = nums[i]
                 
-        if start!=end and f'{start}->{end}' not in res:
+                # Reset the start and end to the current number for a new range.
+                start = nums[i]
+                end = nums[i]
+            else:
+                # Extend the current range by updating the end.
+                end = nums[i]
+        
+        # Add the final range to the result list after the loop ends.
+        if start != end and f'{start}->{end}' not in res:
             res.append(f'{start}->{end}')
         elif f'{end}' not in res:
             res.append(f'{start}')
-        return res
-            
         
+        return res
