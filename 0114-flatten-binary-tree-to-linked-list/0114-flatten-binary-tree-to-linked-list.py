87beb20dba761457d1b13c4ba8d935f62bb353c2
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-17 23:23:07 -0500

    Time: 0 ms (100%), Space: 17.8 MB (9.01%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -4,18 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
-        Do not return anything, modify root in-place instead.
+        Flattens the binary tree into a "linked list" in place, using the tree's right pointers to 
+        represent the list. The order of nodes in the "linked list" follows a pre-order traversal.
+
+        :param root: TreeNode - The root node of the binary tree.
+        :return: None - The tree is modified in-place.
         """
-        curr=root
+        
+        # Start with the current node as the root.
+        curr = root
         while curr:
-            if curr.left!=None:
-                prev=curr.left
+            # If the current node has a left child, we need to process it.
+            if curr.left is not None:
+                # Find the rightmost node in the left subtree (predecessor).
+                prev = curr.left
                 while prev.right:
-                    prev=prev.right
-                prev.right=curr.right
-                curr.right=curr.left
-                curr.left=None
-            curr=curr.right
+                    prev = prev.right
+                
+                # Connect the right subtree of the current node to the rightmost node of the left subtree.
+                prev.right = curr.right
+
+                # Move the left subtree to the right and set the left child to None.
+                curr.right = curr.left
+                curr.left = None
+            
+            # Move to the next node in the modified tree (always moving to the right).
+            curr = curr.right
