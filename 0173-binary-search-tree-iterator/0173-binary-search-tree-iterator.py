5c8195bf92a4db49a6b9d04202a651753903e188
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-18 23:20:51 -0500

    Time: 7 ms (63.2%), Space: 24.9 MB (7.1%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -11,38 +11,32 @@ class BSTIterator:
         Initialize the iterator with the root node of the BST.
         The iterator will traverse the BST in an in-order manner.
         """
-        # Initialize the generator for in-order traversal and get the first value.
-        self.iter = self._inorder(root)
-        self.nxt = next(self.iter, None)  # Store the next element if available.
+        self.stack = []  # Stack to simulate in-order traversal
+        self._pushLeft(root)  # Push all left nodes of the tree into the stack
 
-    def _inorder(self, node: Optional[TreeNode]) -> Generator[int, None, None]:
+    def _pushLeft(self, node: Optional[TreeNode]):
         """
-        Generator function to perform in-order traversal of the BST.
-        :param node: TreeNode - The current node in the BST.
-        :yield: int - The value of the node in in-order sequence.
+        Helper function to push all the left nodes of a tree/subtree to the stack.
         """
-        if node:
-            yield from self._inorder(node.left)  # Traverse the left subtree.
-            yield node.val  # Yield the value of the current node.
-            yield from self._inorder(node.right)  # Traverse the right subtree.
+        while node:
+            self.stack.append(node)
+            node = node.left
 
     def next(self) -> int:
         """
         Return the next smallest number in the BST.
         :return: int - The next in-order value.
         """
-        res, self.nxt = self.nxt, next(self.iter, None)  # Get the current value and update `self.nxt`.
-        return res
+        # Pop the top node from the stack (this is the next smallest element)
+        node = self.stack.pop()
+        # After popping, if the node has a right child, push its leftmost nodes to the stack
+        self._pushLeft(node.right)
+        return node.val
 
     def hasNext(self) -> bool:
         """
         Check if there is a next smallest number in the BST.
         :return: bool - True if there is another value, False otherwise.
         """
-        return self.nxt is not None
-
-
-# Your BSTIterator object will be instantiated and called as such:
-# obj = BSTIterator(root)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+        # If the stack is not empty, we have another node to visit
+        return len(self.stack) > 0
