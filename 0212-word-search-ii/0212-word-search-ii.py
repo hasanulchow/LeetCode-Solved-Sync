0d9aa9b3d8ad71983ec01c95ec2bb299e1cc71ef
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-21 23:18:21 -0500

    Time: 403 ms (97%), Space: 18.7 MB (74.85%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -3,48 +3,55 @@ class Solution:
         
         # Define a DFS function to traverse the board and search for words
         def dfs(x, y, root):
-            # Get the letter at the current position on the board
-            letter = board[x][y]
-            # Traverse the trie to the next node
-            cur = root[letter]
-            # Check if the node has a word in it
-            word = cur.pop('#', False)
+            """
+            A helper function to perform depth-first search on the board to find words.
+            Parameters:
+            - x, y: Current position on the board.
+            - root: Current node in the trie.
+            """
+            letter = board[x][y]  # Get the letter at the current position on the board
+            cur = root[letter]  # Traverse the trie to the next node using the letter
+            word = cur.pop('#', False)  # Check if the node contains a word (denoted by '#')
+            
             if word:
                 # If a word is found, add it to the results list
                 res.append(word)
-            # Mark the current position on the board as visited
-            board[x][y] = '*'
-            # Recursively search in all four directions
+            
+            board[x][y] = '*'  # Mark the current position on the board as visited
+            
+            # Recursively search in all four directions (right, left, down, up)
             for dirx, diry in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 curx, cury = x + dirx, y + diry
-                # Check if the next position is within the board and the next letter is in the trie
+                # Check if the next position is within the board's bounds and the next letter exists in the trie
                 if 0 <= curx < m and 0 <= cury < n and board[curx][cury] in cur:
-                    dfs(curx, cury, cur)
-            # Restore the original value of the current position on the board
-            board[x][y] = letter
-            # If the current node has no children, remove it from the trie
+                    dfs(curx, cury, cur)  # Continue DFS search from the new position
+            
+            board[x][y] = letter  # Restore the original value of the current position on the board
+            
+            # If the current node has no children, remove it from the trie to prune unused nodes
             if not cur:
                 root.pop(letter)
-                
-        # Build a trie data structure from the list of words
+        
+        # Step 1: Build a trie data structure from the list of words
         trie = {}
         for word in words:
             cur = trie
             for letter in word:
-                cur = cur.setdefault(letter, {})
-            cur['#'] = word
+                cur = cur.setdefault(letter, {})  # Add the letter to the trie, create a new node if needed
+            cur['#'] = word  # Mark the end of a word in the trie by using the '#' symbol
             
-        # Get the dimensions of the board
+        # Step 2: Get the dimensions of the board (number of rows and columns)
         m, n = len(board), len(board[0])
-        # Initialize a list to store the results
+        
+        # Step 3: Initialize a list to store the results
         res = []
         
-        # Traverse the board and search for words
+        # Step 4: Traverse the board and start DFS from each cell that has a letter in the trie
         for i in range(m):
             for j in range(n):
-                # Check if the current letter is in the trie
+                # If the current letter is in the trie, begin the DFS search
                 if board[i][j] in trie:
                     dfs(i, j, trie)
         
-        # Return the list of results
+        # Step 5: Return the list of found words
         return res
