6c5c080bfd1ea014aca910f4ef91961bf0717210
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-22 23:10:57 -0500

    Time: 139 ms (47.16%), Space: 59.5 MB (15.59%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,26 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # Result list to store all combinations
         res = []
+        # Temporary list to store the current combination
         comb = []
 
+        # Backtracking function
         def backtrack(start):
+            # If the combination has reached the required size, add it to the result
             if len(comb) == k:
-                res.append(comb[:])
+                res.append(comb[:])  # Append a copy of the current combination
                 return
 
-            for num in range(start, n+1):
+            # Iterate over the numbers from 'start' to 'n'
+            for num in range(start, n + 1):
+                # Add the current number to the combination
                 comb.append(num)
-                backtrack(num+1)
+                # Recursively build the combination with the next number
+                backtrack(num + 1)
+                # Backtrack by removing the last added number
                 comb.pop()
 
+        # Start the backtracking process with the first number
         backtrack(1)
         return res
-        
