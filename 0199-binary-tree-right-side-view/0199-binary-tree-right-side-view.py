164fa00a3a065b9cdedc3334c5374d121434ead7
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-25 13:25:41 -0500

    Time: 0 ms (100%), Space: 17.8 MB (6.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,29 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-        if root is None:
+        """
+        Returns the values of the nodes visible from the right side of the binary tree.
+
+        Args:
+        root (TreeNode): The root of the binary tree.
+
+        Returns:
+        List[int]: A list of integers representing the values visible from the right side.
+        """
+        ans = []  # To store the right-side view of the tree
+        if root is None:  # If the tree is empty, return an empty list
             return ans
 
-        temp = deque([root])
+        # Use a deque for level-order traversal (BFS)
+        temp = deque([root])  # Start with the root node
         while temp:
-            l = len(temp)
-            val = 0
+            l = len(temp)  # Number of nodes in the current level
+            val = 0  # Placeholder for the rightmost node value in the current level
 
+            # Iterate through all nodes in the current level
             for _ in range(l):
-                node = temp.popleft()
-                val = node.val
+                node = temp.popleft()  # Get the front node of the deque
+                val = node.val  # Update the rightmost value (overwritten until last node)
 
+                # Add the left child to the deque if it exists
                 if node.left:
                     temp.append(node.left)
+                # Add the right child to the deque if it exists
                 if node.right:
                     temp.append(node.right)
 
+            # Add the rightmost node's value to the result
             ans.append(val)
 
         return ans
-
-
-        
