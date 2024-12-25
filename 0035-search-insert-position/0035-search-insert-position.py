2339dd88460cc21ca3e3a31b97ea1fdc201502c4
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-25 14:50:52 -0500

    Time: 0 ms (100%), Space: 18.4 MB (8.9%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,16 +1,18 @@
+from typing import List
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
+        left = 0  # Left pointer to the start of the list
+        right = len(nums) - 1  # Right pointer to the end of the list
 
-        while left <= right:
-            mid = (left + right) // 2
+        while left <= right:  # Continue the search while the left pointer is less than or equal to the right pointer
+            mid = (left + right) // 2  # Calculate the middle index
 
-            if nums[mid] == target:
+            if nums[mid] == target:  # If the target is found at mid, return its index
                 return mid
-            elif nums[mid] > target:
+            elif nums[mid] > target:  # If the target is smaller than nums[mid], narrow the search to the left half
                 right = mid - 1
-            else:
+            else:  # If the target is larger than nums[mid], narrow the search to the right half
                 left = mid + 1
-        
-        return left
+
+        return left  # If the target is not found, return the left pointer, which indicates the insert position
