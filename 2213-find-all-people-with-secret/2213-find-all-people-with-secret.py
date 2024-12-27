dfbfe998f71da3cbb79cd8b875525ee3286b3040
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-27 00:22:55 -0500

    Time: 1397 ms (6.36%), Space: 79.3 MB (9.54%) - LeetHub

---
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        g={i:[] for i in range(n)}
+        for (src,dst,wt) in meetings: 
+            g[src].append((dst,wt))
+            g[dst].append((src,wt))
+            
+        q=[(0,0),(firstPerson,0)]
+        v={}
+        while q:
+            node,time=q.pop(0)
+            if node in v and v[node]<=time: continue
+            v[node]=time
+            for (n,t) in g[node]:
+                if t<time or (n in v and v[n]<=t):continue
+                q.append((n,t))
+        return list(v)
