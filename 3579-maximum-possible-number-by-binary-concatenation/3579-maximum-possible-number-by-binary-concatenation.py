29ba210242395ab931947286a12e599c25cb7935
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2024-12-27 21:40:25 -0500

    Time: 15 ms (5.39%), Space: 17.8 MB (6.73%) - LeetHub

--- 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.py
+++ 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.py
@@ -1,4 +1,41 @@
 class Solution:
-    def maxGoodNumber(self, A):
-        return (lambda a: max(int(''.join(c), 2) for c in permutations(a, 3)))(map(lambda s:s[2:],map(bin, A)))
+    def maxGoodNumber(self, nums: List[int]) -> int:
+        
+        def num_to_bin(num):
+            s = ''
+            while num:
+                if num & 1:
+                    s += '1'
+                else:
+                    s += '0'
+                num = num >> 1
+            return s[::-1]
+            
+        def bin_to_num(s):
+            num = 0
+            for idx, char in enumerate(s):
+                num = num << 1
+                num = num | int(char)
+            return num
+
+        nums = [num_to_bin(x) for x in nums]
+
+        def dfs(s, picked=set()):
+            if len(picked) == len(nums):
+                possible.append(s)
+                return
+
+            for i in range(len(nums)):
+                if i in picked:
+                    continue
+                picked.add(i)
+                dfs(s + nums[i], picked)
+                picked.remove(i)
+
+        possible = []
+        dfs('', set())
+
+        numbers = [bin_to_num(x) for x in possible]
+        
+        return max(numbers)
         
