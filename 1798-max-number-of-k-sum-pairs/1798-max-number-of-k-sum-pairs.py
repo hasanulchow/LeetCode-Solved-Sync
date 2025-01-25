a185e81552aa29c4cc986f193e1f78de971fa6eb
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2025-01-24 23:17:41 -0500

    Time: 919 ms (5.01%), Space: 37.5 MB (7.12%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -1,15 +1,43 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        ans, i, j = 0, 0, len(nums) - 1
-        while i < j:
-            if nums[i] + nums[j] == k:
-                ans += 1
-                i += 1
-                j -= 1
-            elif nums[i] + nums[j] > k:
-                j -= 1
-            elif nums[i] + nums[j] < k:
-                i += 1
-        return ans
+        i = 0
+        j = 1
+
+        """
+        n^2 is 1,000,000,000 so no n^2
+
+        brute force would take too long
         
+        this is a variant of two sum.
+        if we store the two sum, we know what indices we can immediately remove
+        """
+        h = defaultdict(list)
+        for ni, n in enumerate(nums):
+            h[n].append(ni)
+
+        
+        if len(h) == 1:  # edge case: all same number, like k=10, [5,5,5,5.........5,5]
+            if list(h.keys())[0] == k / 2:
+                return len(nums) // 2
+            return 0
+
+        used = set()  # no need to actually remove elements which would mess up all the indices in h
+        ops = 0
+        i = 0
+        while nums and i < len(nums):
+            if i in used:
+                i += 1
+                continue
+
+            target = k - nums[i]
+            if h.get(target) != []:
+                for ind_i, ind in enumerate(h[target]):
+                    if ind > i and ind not in used:
+                        ops += 1
+                        used.add(i)
+                        used.add(ind)
+                        break
+            
+            i += 1
+        
+        return ops
