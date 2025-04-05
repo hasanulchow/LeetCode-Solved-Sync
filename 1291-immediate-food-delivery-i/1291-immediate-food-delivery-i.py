c91e0d60b241b0dc5757fba6f2c74028f8f13512
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2025-04-05 16:32:48 -0400

    Time: 291 ms (84.57%), Space: 67.2 MB (6.17%) - LeetHub

---
+++ 1291-immediate-food-delivery-i/1291-immediate-food-delivery-i.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    df = delivery[delivery. order_date == delivery.customer_pref_delivery_date]
+    percentage = round(df.shape[0]*100/delivery.shape[0],2)
+    result_df = pd.DataFrame({'immediate_percentage':[percentage]})
+    return result_df
