13edd0bd0cf8839b7b5639cc46b0782a52e8dd20
Mohammed Hasanul Chowdhury <hasanulrafi46@gmail.com>
2025-04-07 00:19:58 -0400

    Time: 481 ms (75.24%), Space: 17.9 MB (84.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,29 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
-        if s&1:
-            return False
-        """
-        The dp array stores the total obtained sums we have come across so far.
-        Notice that dp[0] = True; if we never select any element, the total sum is 0.
-        """
-        dp = [True]+[False]*s
-        # Now, loop through each element
-        for num in nums:
-            for curr in range(s, num-1, -1):  # avoid going out-of-bounds
-                """
-                Case 1: The current sum (curr) has been seen before.
-                        Then, if we don't select the current element, the sum will not change.
-						So, this total sum will still exist, and its dp value remains True.
-				
-				Case 2: The current sum (curr) has not been seen before,
-				        but it can be obtained by selecting the current element.
-						This means that dp[curr-num] = True, and thus dp[curr] now becomes True.
-				
-				Case 3: The current sum (curr) has not been seen before,
-				        and it cannot be obtained by selecting the current element.
-						So, this total sum will still not exist, and its dp value remains False.
-                """
-                dp[curr] = dp[curr] or dp[curr-num]
-        # Finally, we want to obtain the target sum
-        return dp[s//2]  # or dp[s>>1]
+        # we can partition the array into two subsets with equal sum
+        # only when the sum is even
+        if s % 2: return False
+        # k is our target sum
+        k = s // 2
+        # subset sum problem -> solve it with DP
+        dp = [True] + [False] * k
+        # for each number
+        for x in nums:
+            # check all number from k to x
+            # to check if adding x can achieve i
+            for i in range(k, x - 1, -1): dp[i] |= dp[i - x]
+            # if dp[k] is achievable at a point, 
+            # then we can return True
+            if dp[k]: return True
+        return dp[k]
