31c0de493bd91ea7b1132f4fc8dac77d82b32773
Hasanul Chowdhury <hasanulrafi46@gmail.com>
2025-05-20 13:51:08 -0400

    Time: 19 ms (37.16%), Space: 22.5 MB (34.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,18 @@
-from collections import defaultdict
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Create a defaultdict with a default type of list to store grouped anagrams
-        ans = defaultdict(list)
+#step:1
+        group_anagram = {}
+        for word in strs:
+            count = [0]*26
 
-        # Iterate over each string in the input list
-        for s in strs:
-            # Sort the characters of the string to generate a key that represents its anagram group
-            key = "".join(sorted(s))
-            # Append the original string to the list corresponding to the sorted key
-            ans[key].append(s)
+            for char in word:
+                count[ord(char)-ord('a')] +=1
 
-        # Convert the defaultdict values to a list and return it
-        return list(ans.values())
+            key = tuple(count)
+            if key in group_anagram:
+                group_anagram[key].append(word)
+
+            else:
+                group_anagram[key] = [word]
+
+        return list(group_anagram.values())
